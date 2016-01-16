b0d820093e12b3c7c5b36bcd65da9e48fd65bdd1
Ivo Petkov <ivo@ivopetkov.com>
2016-01-16 08:46:44 +0200

    Context loadFile() function

--- src/App/Context.php
+++ src/App/Context.php
@@ -11,4 +11,16 @@ class Context
      */
     public $dir = '';
 
+    /**
+     * 
+     * @global \App $app
+     * @param string $filename
+     * @return boolean
+     */
+    function loadFile($filename)
+    {
+        global $app;
+        return $app->load($this->dir . $filename);
+    }
+
 }
