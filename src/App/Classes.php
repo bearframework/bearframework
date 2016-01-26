b6009c644866d9ba292eefe0c40ecfe368c0a6f0
Ivo Petkov <ivo@ivopetkov.com>
2016-01-26 08:09:00 +0200

    Classes API

---
+++ src/App/Classes.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * Bear Framework
+ * http://bearframework.com
+ * Copyright (c) 2016 Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
+namespace App;
+
+/**
+ * Provides functionaly for autoloading classes
+ */
+class Classes
+{
+
+    /**
+     * Registered classes
+     * @var array 
+     */
+    private $data = [];
+
+    /**
+     * Registers a class for autoloading
+     * @param string $class The class name
+     * @param string $filename The filename that contains the class
+     * @throws \InvalidArgumentException
+     * @return void No value is returned
+     */
+    function add($class, $filename)
+    {
+        if (!is_string($class)) {
+            throw new \InvalidArgumentException('');
+        }
+        if (!is_string($filename)) {
+            throw new \InvalidArgumentException('');
+        }
+        $this->data[$class] = $filename;
+    }
+
+    /**
+     * Loads a class if registered
+     * @param string $class
+     * @throws \InvalidArgumentException
+     * @return void No value is returned
+     */
+    function load($class)
+    {
+        if (!is_string($class)) {
+            throw new \InvalidArgumentException('');
+        }
+        $app = &\App::$instance;
+        if (isset($this->data[$class])) {
+            $app->load($this->data[$class]);
+        }
+    }
+
+}
---
+++ src/App/Context/Classes.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * Bear Framework
+ * http://bearframework.com
+ * Copyright (c) 2016 Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
+namespace App\Context;
+
+/**
+ * Provides functionaly for autoloading classes in the current context
+ */
+class Classes
+{
+
+    /**
+     *
+     * @var string 
+     */
+    private $dir = '';
+
+    /**
+     * The constructor
+     * @param string $dir The directory where the current addon or application are located 
+     * @throws \InvalidArgumentException
+     * @return void No value is returned
+     */
+    function __construct($dir)
+    {
+        if (!is_string($dir)) {
+            throw new \InvalidArgumentException('');
+        }
+        $this->dir = $dir;
+    }
+
+    /**
+     * Registers a class for autoloading in the current context
+     * @param string $class The class name
+     * @param string $filename The filename that contains the class
+     * @throws \InvalidArgumentException
+     * @return void No value is returned
+     */
+    function add($class, $filename)
+    {
+        if (!is_string($class)) {
+            throw new \InvalidArgumentException('');
+        }
+        if (!is_string($filename)) {
+            throw new \InvalidArgumentException('');
+        }
+        $app = &\App::$instance;
+        $app->classes->add($class, $this->dir . $filename);
+    }
+
+}
