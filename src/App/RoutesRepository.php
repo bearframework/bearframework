bbf5a6ee7a89e66cdc437952ba6dcfbfa46d6f02
Ivo Petkov <ivo@ivopetkov.com>
2017-01-31 15:09:09 +0200

    Added support for multiple callbacks in $app->routes->add();

--- src/App/RoutesRepository.php
+++ src/App/RoutesRepository.php
@@ -28,35 +28,49 @@ class RoutesRepository
      * Registers a request handler.
      * 
      * @param string|string[] $pattern Path pattern. Can contain "?" (path segment) and "*" (matches everything).
-     * @param callable $callback Function that is expected to return object of type \BearFramework\App\Response.
+     * @param callable|callable[] $callback Function that is expected to return object of type \BearFramework\App\Response.
      * @param array $options Matching options for methods (GET, HEAD, POST, DELETE, PUT, PATCH, OPTIONS) and protocols (HTTP, HTTPS).
      * @throws \InvalidArgumentException
      * @return \BearFramework\App\RoutesRepository Returns reference to itself.
      */
-    public function add($pattern, callable $callback, array $options = ['GET']): \BearFramework\App\RoutesRepository
+    public function add($pattern, $callback, array $options = ['GET']): \BearFramework\App\RoutesRepository
     {
         if (is_string($pattern)) {
             if (!isset($pattern{0})) {
-                throw new \InvalidArgumentException('The route argument must be a not empty string or array of not empty strings');
+                throw new \InvalidArgumentException('The pattern argument must be a not empty string or array of not empty strings');
             }
-            $this->data[] = [[$pattern], $callback, $options];
-            return $this;
+            $pattern = [$pattern];
         } elseif (is_array($pattern)) {
             if (empty($pattern)) {
-                throw new \InvalidArgumentException('The route argument must be a not empty string or array of not empty strings');
+                throw new \InvalidArgumentException('The pattern argument must be a not empty string or array of not empty strings');
             }
             foreach ($pattern as $_pattern) {
                 if (!is_string($_pattern)) {
-                    throw new \InvalidArgumentException('The route argument must be a not empty string or array of not empty strings');
+                    throw new \InvalidArgumentException('The pattern argument must be a not empty string or array of not empty strings');
                 }
                 if (!isset($_pattern{0})) {
-                    throw new \InvalidArgumentException('The route argument must be a not empty string or array of not empty strings');
+                    throw new \InvalidArgumentException('The pattern argument must be a not empty string or array of not empty strings');
                 }
             }
-            $this->data[] = [$pattern, $callback, $options];
-            return $this;
+        } else {
+            throw new \InvalidArgumentException('The pattern argument must be a not empty string or array of not empty strings');
         }
-        throw new \InvalidArgumentException('The route argument must be a not empty string or array of not empty strings');
+        if (is_callable($callback)) {
+            $callback = [$callback];
+        } elseif (is_array($callback)) {
+            if (empty($callback)) {
+                throw new \InvalidArgumentException('The callback argument must be a valid callable or array of valid callables');
+            }
+            foreach ($callback as $_callback) {
+                if (!is_callable($_callback)) {
+                    throw new \InvalidArgumentException('The callback argument must be a valid callable or array of valid callables');
+                }
+            }
+        } else {
+            throw new \InvalidArgumentException('The callback argument must be a valid callable or array of valid callables');
+        }
+        $this->data[] = [$pattern, $callback, $options];
+        return $this;
     }
 
     /**
@@ -95,16 +109,18 @@ class RoutesRepository
                     }
                 }
                 if ($found) {
-                    ob_start();
-                    try {
-                        $response = call_user_func($route[1]);
-                        ob_end_clean();
-                    } catch (\Exception $e) {
-                        ob_end_clean();
-                        throw $e;
-                    }
-                    if ($response instanceof App\Response) {
-                        return $response;
+                    foreach ($route[1] as $callable) {
+                        ob_start();
+                        try {
+                            $response = call_user_func($callable);
+                            ob_end_clean();
+                        } catch (\Exception $e) {
+                            ob_end_clean();
+                            throw $e;
+                        }
+                        if ($response instanceof App\Response) {
+                            return $response;
+                        }
                     }
                     // continue searching
                 }
