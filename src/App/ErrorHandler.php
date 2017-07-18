e3026033f68049ee323b78e1ba8c7f33cf754f31
Ivo Petkov <ivo@ivopetkov.com>
2017-07-18 07:20:39 +0300

    Simplified errors log and added additional log for error details.

--- src/App/ErrorHandler.php
+++ src/App/ErrorHandler.php
@@ -47,22 +47,36 @@ class ErrorHandler
      * @param string $message
      * @param string $file
      * @param int $line
-     * @param array $trace
+     * @param array $backtrace
      * @return void No value is returned.
      */
-    static function handleError(string $message, string $file, int $line, array $trace): void
+    static function handleError(string $message, string $file, int $line, array $backtrace): void
     {
         $app = App::get();
+
+        $simpleBacktrace = [];
+        foreach ($backtrace as $backtraceRow) {
+            $simpleBacktrace[] = (isset($backtraceRow['file']) ? $backtraceRow['file'] : '') . ':' . (isset($backtraceRow['line']) ? $backtraceRow['line'] : '');
+        }
+        
+        $id = uniqid();
+
+        $data = $message . ' in ' . $file . ':' . $line;
+        $data .= "\n\nError ID:\n" . $id;
+        $data .= "\n\nRequest:\n" . $app->request->method . ' ' . $app->request->base . $app->request->path;
+        $data .= "\n\nSimple backtrace:\n" . implode("\n", $simpleBacktrace);
+        $simpleData = $data;
+        $data .= "\n\nPHP variables:\n" . print_r([
+                    'GET' => $_GET,
+                    'POST' => $_POST,
+                    'SERVER' => $_SERVER
+                        ], true);
+        $data .= "\n\nFull backtrace:\n" . print_r($backtrace, true);
+
         if ($app->config->logErrors) {
             try {
-                $data = [];
-                $data['file'] = $file;
-                $data['line'] = $line;
-                $data['trace'] = $trace;
-                $data['GET'] = isset($_GET) ? $_GET : null;
-                $data['POST'] = isset($_POST) ? $_POST : null;
-                $data['SERVER'] = isset($_SERVER) ? $_SERVER : null;
-                $app->logger->log('error', $message, $data);
+                $app->logger->log('error', $simpleData);
+                $app->logger->log('error-' . $id, $data);
             } catch (\Exception $e) {
                 
             }
@@ -71,15 +85,8 @@ class ErrorHandler
             ob_clean();
         }
         if ($app->config->displayErrors) {
-            $data = "Error:";
-            $data .= "\nMessage: " . $message;
-            $data .= "\nFile: " . $file;
-            $data .= "\nLine: " . $line;
-            $data .= "\nTrace: " . print_r($trace, true);
-            $data .= "\nGET: " . print_r(isset($_GET) ? $_GET : null, true);
-            $data .= "\nPOST: " . print_r(isset($_POST) ? $_POST : null, true);
-            $data .= "\nSERVER: " . print_r(isset($_SERVER) ? $_SERVER : null, true);
             http_response_code(503);
+            echo "Error occurred:\n\n";
             echo $data;
             exit;
         } else {
