12664b7d2a7b294b0f32351fff545dfef77b6b8d
Ivo Petkov <ivo@ivopetkov.com>
2017-10-23 09:08:17 +0300

    Added check for deprecated mbstring.func_overload in PHP 7.2.

--- src/App.php
+++ src/App.php
@@ -227,7 +227,9 @@ class App
         if (!$this->initialized) {
             // @codeCoverageIgnoreStart
             if ($this->config->updateEnvironment) {
-                ini_set('mbstring.func_overload', 7);
+                if (version_compare(PHP_VERSION, '7.2.0', '<')) {
+                    ini_set('mbstring.func_overload', 7);
+                }
                 ini_set("pcre.backtrack_limit", 100000000);
                 ini_set("pcre.recursion_limit", 100000000);
             }
