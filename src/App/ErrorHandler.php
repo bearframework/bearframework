ae5d4756bcd2cebbd81bfebb5e72f7f9a0425432
Ivo Petkov <ivo@ivopetkov.com>
2018-10-15 10:11:43 +0300

    App\ErrorHandler::handleError() is removed. Added support for multiple errors (multiple exceptions).

--- src/App/ErrorHandler.php
+++ src/App/ErrorHandler.php
@@ -25,7 +25,15 @@ class ErrorHandler
      */
     static function handleException(\Throwable $exception): void
     {
-        self::handleError($exception->getMessage(), $exception->getFile(), $exception->getLine(), $exception->getTrace());
+        $errors = [];
+        while ($exception instanceof \Exception) {
+            $message = 'Exception: ' . $exception->getMessage() . ' in ' . $exception->getFile() . ':' . $exception->getLine();
+            $trace = $exception->getTrace();
+            $errors[] = ['message' => $message, 'trace' => $trace];
+            $exception = $exception->getPrevious();
+        }
+        $errors = array_reverse($errors);
+        self::handleErrors($errors);
     }
 
     /**
@@ -39,44 +47,66 @@ class ErrorHandler
             ob_end_clean();
         }
         $messageParts = explode(' in ' . $errorData['file'] . ':' . $errorData['line'], $errorData['message'], 2);
-        self::handleError(trim($messageParts[0]), $errorData['file'], (int) $errorData['line'], isset($messageParts[1]) ? [trim(str_replace('Stack trace:', '', $messageParts[1]))] : []);
+        $message = 'Fatal error: ' . trim($messageParts[0]) . ' in ' . $errorData['file'] . ':' . (int) $errorData['line'];
+        $trace = isset($messageParts[1]) ? [trim(str_replace('Stack trace:', '', $messageParts[1]))] : [];
+        self::handleErrors([['message' => $message, 'trace' => $trace]]);
     }
 
     /**
      * 
-     * @param string $message
-     * @param string $file
-     * @param int $line
-     * @param array $backtrace
+     * @param array $errors
      * @return void No value is returned.
      */
-    static function handleError(string $message, string $file, int $line, array $backtrace): void
+    static private function handleErrors(array $errors): void
     {
         $app = App::get();
+        $logKey = uniqid();
 
-        $simpleBacktrace = [];
-        foreach ($backtrace as $backtraceRow) {
-            $simpleBacktrace[] = (isset($backtraceRow['file']) ? $backtraceRow['file'] : '') . ':' . (isset($backtraceRow['line']) ? $backtraceRow['line'] : '');
+        $simpleLog = '';
+        $fullLog = '';
+        $addContent = function(string $title, string $data, bool $addToSimpleLog, bool $addToFullLog) use (&$simpleLog, &$fullLog) {
+            if ($addToSimpleLog) {
+                if (isset($simpleLog[0])) {
+                    $simpleLog .= "\n\n";
+                }
+                $simpleLog .= $title . ':' . (isset($data[0]) ? "\n" . trim($data) : '');
+            }
+            if ($addToFullLog) {
+                if (isset($fullLog[0])) {
+                    $fullLog .= "\n\n";
+                }
+                $fullLog .= $title . ':' . (isset($data[0]) ? "\n" . trim($data) : '');
+            }
+        };
+        $addContent('Log key', $logKey, true, false);
+
+        $errorsCount = sizeof($errors);
+        foreach ($errors as $i => $error) {
+            $message = isset($error['message']) ? $error['message'] : '';
+            $trace = isset($error['trace']) ? $error['trace'] : [];
+
+            $simpleTrace = [];
+            foreach ($trace as $traceRow) {
+                $simpleTrace[] = (isset($traceRow['file']) ? $traceRow['file'] : '') . ':' . (isset($traceRow['line']) ? $traceRow['line'] : '');
+            }
+            if ($errorsCount > 1) {
+                $addContent('Error ' . ($i + 1) . ' of ' . $errorsCount, '', true, true);
+            }
+            $addContent('Message', $message, true, true);
+            $addContent('Simple trace', implode("\n", $simpleTrace), true, true);
+            $addContent('Full trace', print_r($trace, true), false, true);
         }
-
-        $id = uniqid();
-
-        $data = $message . ' in ' . $file . ':' . $line;
-        $data .= "\n\nError ID:\n" . $id;
-        $data .= "\n\nRequest:\n" . $app->request->method . ' ' . $app->request->base . $app->request->path;
-        $data .= "\n\nSimple backtrace:\n" . implode("\n", $simpleBacktrace);
-        $simpleData = $data;
-        $data .= "\n\nPHP variables:\n" . print_r([
-                    'GET' => $_GET,
-                    'POST' => $_POST,
-                    'SERVER' => $_SERVER
-                        ], true);
-        $data .= "\n\nFull backtrace:\n" . print_r($backtrace, true);
+        $addContent('Request', $app->request->method . ' ' . $app->request->base . $app->request->path, true, true);
+        $addContent('PHP variables', print_r([
+            'GET' => $_GET,
+            'POST' => $_POST,
+            'SERVER' => $_SERVER
+                        ], true), false, true);
 
         if ($app->config->logErrors) {
             try {
-                $app->logger->log('error', $simpleData);
-                $app->logger->log('error-' . $id, $data);
+                $app->logger->log('error', $simpleLog);
+                $app->logger->log('error-' . $logKey, $fullLog);
             } catch (\Exception $e) {
                 
             }
@@ -86,8 +116,8 @@ class ErrorHandler
         }
         if ($app->config->displayErrors) {
             http_response_code(503);
-            echo "Error occurred:\n\n";
-            echo $data;
+            echo (($errorsCount > 1) ? $errorsCount . ' errors occurred:' : 'Error occurred:') . "\n\n";
+            echo $fullLog;
         } else {
             $response = new App\Response\TemporaryUnavailable();
             try {
