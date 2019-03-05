dcfa4420a001554c972f17ec72118b24d85b675d
Ivo Petkov <ivo@ivopetkov.com>
2019-03-05 21:56:01 +0200

    Error handler optimizations.

--- src/Internal/ErrorHandler.php
+++ src/Internal/ErrorHandler.php
@@ -62,77 +62,100 @@ class ErrorHandler
      */
     static private function handleErrors(\BearFramework\App $app, array $errors, $options): void
     {
-        $logErrors = isset($options['logErrors']) && (bool) $options['logErrors'] === true;
-        $displayErrors = isset($options['displayErrors']) && (bool) $options['displayErrors'] === true;
-
-        $logKey = uniqid();
-
-        $simpleLog = '';
-        $fullLog = '';
-        $addContent = function(string $title, string $data, bool $addToSimpleLog, bool $addToFullLog) use (&$simpleLog, &$fullLog) {
-            if ($addToSimpleLog) {
-                if (isset($simpleLog[0])) {
-                    $simpleLog .= "\n\n";
-                }
-                $simpleLog .= $title . ':' . (isset($data[0]) ? "\n" . trim($data) : '');
-            }
-            if ($addToFullLog) {
-                if (isset($fullLog[0])) {
-                    $fullLog .= "\n\n";
-                }
-                $fullLog .= $title . ':' . (isset($data[0]) ? "\n" . trim($data) : '');
-            }
-        };
-        $addContent('Log key', $logKey, true, false);
-
-        $errorsCount = sizeof($errors);
-        foreach ($errors as $i => $error) {
-            $message = isset($error['message']) ? $error['message'] : '';
-            $trace = isset($error['trace']) ? $error['trace'] : [];
-
-            $simpleTrace = [];
-            foreach ($trace as $traceRow) {
-                $simpleTrace[] = (isset($traceRow['file']) ? $traceRow['file'] . ':' . (isset($traceRow['line']) ? $traceRow['line'] : '') : '...');
-            }
-            if ($errorsCount > 1) {
-                $addContent('Error ' . ($i + 1) . ' of ' . $errorsCount, '', true, true);
-            }
-            $addContent('Message', $message, true, true);
-            $addContent('Simple trace', implode("\n", $simpleTrace), true, true);
-            $addContent('Full trace', print_r($trace, true), false, true);
-        }
-        $addContent('Request', $app->request->method . ' ' . $app->request->base . $app->request->path, true, true);
-        $addContent('PHP variables', print_r([
-            'GET' => $_GET,
-            'POST' => $_POST,
-            'SERVER' => $_SERVER
-                        ], true), false, true);
-
-        if ($logErrors) {
-            try {
-                $app->logs->log('error', $simpleLog);
-                $app->logs->log('error-' . $logKey, $fullLog);
-            } catch (\Exception $e) {
-                
-            }
-        }
-
         $level = ob_get_level();
         for ($i = 0; $i < $level; $i++) {
             ob_end_clean();
         }
-        if ($displayErrors) {
-            http_response_code(503);
-            echo (($errorsCount > 1) ? $errorsCount . ' errors occurred:' : 'Error occurred:') . "\n\n";
-            echo $fullLog;
-        } else {
-            $response = new App\Response\TemporaryUnavailable();
-            try {
-                $app->send($response);
-            } catch (\Exception $e) {
-                http_response_code(503);
-                echo 'Temporary Unavailable';
+
+        $logErrors = isset($options['logErrors']) && (bool) $options['logErrors'] === true;
+        $displayErrors = isset($options['displayErrors']) && (bool) $options['displayErrors'] === true;
+
+        if ($logErrors || $displayErrors) {
+            if ($logErrors) {
+                $logKey = uniqid();
             }
+            for ($i = 0; $i < 3; $i++) {
+                if (!$logErrors && $i !== 2) {
+                    continue;
+                }
+                if (!$displayErrors && $i === 2) {
+                    continue;
+                }
+
+                $isSimpleLog = $i === 0 || $i === 2;
+                $log = '';
+                $addContent = function(string $title, string $data) use (&$log) {
+                    if (isset($log[0])) {
+                        $log .= "\n\n";
+                    }
+                    $log .= $title . ':' . (isset($data[0]) ? "\n" . trim($data) : '');
+                };
+                if ($logErrors && $isSimpleLog) {
+                    $addContent('Log key', $logKey);
+                }
+
+                $errorsCount = sizeof($errors);
+                foreach ($errors as $j => $error) {
+                    $message = isset($error['message']) ? $error['message'] : '';
+                    $trace = isset($error['trace']) ? $error['trace'] : [];
+
+                    $simpleTrace = [];
+                    foreach ($trace as $traceRow) {
+                        $simpleTrace[] = (isset($traceRow['file']) ? $traceRow['file'] . ':' . (isset($traceRow['line']) ? $traceRow['line'] : '') : '...');
+                    }
+                    if ($errorsCount > 1) {
+                        $addContent('Error ' . ($j + 1) . ' of ' . $errorsCount, '');
+                    }
+                    $addContent('Message', $message);
+                    $addContent('Simple trace', implode("\n", $simpleTrace));
+                    if (!$isSimpleLog) {
+                        $addContent('Full trace', print_r($trace, true));
+                    }
+                }
+
+                $addContent('Request', $app->request->method . ' ' . $app->request->base . $app->request->path);
+                if (!$isSimpleLog) {
+                    $addContent('PHP variables', print_r(
+                                    [
+                                        'GET' => $_GET,
+                                        'POST' => $_POST,
+                                        'SERVER' => $_SERVER
+                                    ], true)
+                    );
+                }
+
+                if ($i !== 2) {
+                    if ($logErrors) {
+                        try {
+                            if ($isSimpleLog) {
+                                $app->logs->log('error', $log);
+                            } else {
+                                $app->logs->log('error-' . $logKey, $log);
+                            }
+                        } catch (\Exception $e) {
+                            
+                        }
+                    }
+                } else {
+                    if ($displayErrors) {
+                        http_response_code(503);
+                        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+                        header("Pragma: no-cache");
+                        echo (($errorsCount > 1) ? $errorsCount . ' errors occurred:' : 'Error occurred:') . "\n\n";
+                        echo $log;
+                        exit;
+                    }
+                }
+            }
+        }
+        
+        try {
+            $app->send(new App\Response\TemporaryUnavailable());
+        } catch (\Exception $e) {
+            http_response_code(503);
+            header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+            header("Pragma: no-cache");
+            echo 'Temporarily Unavailable';
         }
         exit;
     }
